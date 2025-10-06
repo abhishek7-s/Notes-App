ffcead4ec158291c1858fa96a61790a4e0b9ca8f
Abhishek <abhisheksharma7124@gmail.com>
2025-10-06 18:11:28 +0530

    fix minor error

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://setnotes.vercel.app/',
+    origin: 'https://setnotes.vercel.app',
   });
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3001);
