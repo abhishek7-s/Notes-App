abd45fdb421d2507db71df0d395889bc7bf3561e
Abhishek <abhisheksharma7124@gmail.com>
2025-10-06 17:11:23 +0530

    fix build error

--- backend/src/firebase/firebase.service.ts
+++ backend/src/firebase/firebase.service.ts
@@ -7,9 +7,23 @@ export class FirebaseService implements OnModuleInit {
   private readonly adminApp: admin.app.App;
   
   constructor() {
+    if (
+      !process.env.FIREBASE_PROJECT_ID ||
+      !process.env.FIREBASE_CLIENT_EMAIL ||
+      !process.env.FIREBASE_PRIVATE_KEY
+    ) {
+      throw new Error('Firebase environment variables are not set.');
+    }
+
+    const serviceAccount = {
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    } as admin.ServiceAccount;
+    
     if (!admin.apps.length) {
       this.adminApp = admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+        credential: admin.credential.cert(serviceAccount),
       });
     } else {
       this.adminApp = admin.app();
